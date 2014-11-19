1f71c708a5d6fdaf1dc4bb29530f93a6e9da1f79
Sobranie <ywq1991@gmail.com>
2014-11-19 18:04:59 +0800

    compass sprite debug done

--- father/style/config.rb
+++ father/style/config.rb
@@ -2,10 +2,11 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "../.." 
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "i"
+sprite_load_path = "sass/sp"
 #javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
--- template/test/index.php
+++ template/test/index.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html>
-<title>
-
+<head>
+<link rel="stylesheet" href="../../style/css/test.css">
+</head>
+<body>
+<div class='mycool-icon'></div>
+</body>
 </html>
